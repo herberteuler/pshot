e3b02f6104ea89e5f1c2f24e1d2441882ba9d09c
Guanpeng Xu <herberteuler@gmail.com>
2017-05-29 06:12:00 +0800

    Change time unit

--- src/main/scala/pshot/Main.scala
+++ src/main/scala/pshot/Main.scala
@@ -21,7 +21,7 @@ object Main extends App {
 
   val dirNameFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
-  val fileNameFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+  val fileNameFormatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS")
 
   def takeScreenshot(robot: Robot, rect: Rectangle, root: File,
                      format: String): Unit = {
@@ -45,7 +45,7 @@ object Main extends App {
   val rect = new Rectangle(Toolkit.getDefaultToolkit.getScreenSize)
   val root = conf.get[File]("pshot.root").toOption.get
   val format = conf.get[String]("pshot.format").toOption.get
-  val period = conf.get[Int]("pshot.period-mins").toOption.get * 60000
+  val period = conf.get[Int]("pshot.period-secs").toOption.get * 1000
   timer.scheduleAtFixedRate(takeScreenshot(robot, rect, root, format), 0, period)
 
   val latch = {
